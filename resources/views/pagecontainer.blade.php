b9f14f78e66921596e7d29655817b256620eb441
jovialcore <jollydosky@gmail.com>
2021-03-23 10:18:24 +0100

    inital issue of css not loading..fixing up

--- resources/views/pagecontainer.blade.php
+++ resources/views/pagecontainer.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CAR-VALUATE</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/fontawesome-all.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/login-style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/login-style.css') }}">
 
     <!-- Custom Style -->
     <link rel="stylesheet" href="{{ secure_asset('css/main.css') }}">
